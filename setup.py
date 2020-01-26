ca7c61d17fc03dfcad0f9bd14859f59db6fd8c17
Sarmad Gulzar <sarmadgulzar@outlook.com>
2020-01-26 18:10:10 +0500

    Added files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pyutilx',
+  packages = ['pyutilx'],
+  version = '0.1',
+  license = 'MIT',
+  description = 'Useful Python Functions',
+  author = 'Sarmad Gulzar',
+  author_email = 'sarmadgulzar@outlook.com',
+  url = 'https://github.com/sarmadgulzar/pyutilx',
+  download_url = 'https://github.com/sarmadgulzar/pyutilx/archive/v_01.tar.gz',
+  keywords = ['PYTHON', 'UTILITIES'],
+  install_requires=[
+      ],
+)
